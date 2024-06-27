c8e5de5444316f286c1c2028bfc9ce39eb763321
frantic03117 <shahrukh@franticpro.com>
2024-06-27 11:27:35 +0530

    main

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:5000/api/v1/";
-export const base_url_img = "http://localhost:5000/";
+export const base_url = "https://electronics-backend-jhe8.onrender.com/api/v1/";
+export const base_url_img = "https://electronics-backend-jhe8.onrender.com/";
